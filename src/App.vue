e3d6a2a0b962ee5ea4e20d16bf4635411a4e84c2
unknown <ramovic225@gmail.com>
2025-01-10 20:21:26 +0100

    alphabet added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
+  <HomeView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+</script>
+
+<template>
+    <div class="flex justify-center gap-4">
+        <div v-for="letter in letters" :key="letter" class="text-xl cursor-pointer">
+            {{ letter }}
+        </div>
+    </div>
+</template>
