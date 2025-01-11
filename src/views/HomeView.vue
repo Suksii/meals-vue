d8a50d28d9961f62f1fb4d74c87d08a0699de397
unknown <ramovic225@gmail.com>
2025-01-11 11:07:38 +0100

    category slider added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,14 @@
 import { onMounted, ref } from 'vue';
 import { request } from '../api';
 
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Autoplay } from 'swiper/modules';
+
+import 'swiper/css';
+
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
 const categories = ref([]);
+const modules = [Autoplay]
 
 onMounted(async () => {
     try {
@@ -16,6 +22,13 @@ onMounted(async () => {
     }
 })
 
+const onSwiper = (swiper) => {
+    console.log(swiper);
+}
+const onSlideChange = () => {
+    console.log('slide change');
+};
+
 </script>
 
 <template>
@@ -24,12 +37,17 @@ onMounted(async () => {
             {{ letter }}
         </router-link>
     </div>
-    <div class="flex justify-center gap-4">
-        <div v-for="category in categories" :key="category.idCategory"
-            class="flex flex-col items-center justify-center gap-4">
-            <img :src="category.strCategoryThumb" :alt="category.strCategory"
-                class="rounded-full h-24 w-24 object-cover shrink-0" />
-            <h3 class="text-xl font-semibold uppercase">{{ category.strCategory }}</h3>
-        </div>
+    <div class="flex justify-center gap-4 md:w-[80%] px-4 mx-auto">
+        <swiper :modules="modules" :slides-per-view="6" :space-between="50" :loop="true"
+            :autoplay="{ delay: 1, disableOnInteraction: false }" :speed="2000" @swiper="onSwiper"
+            @slideChange="onSlideChange">
+            <swiper-slide v-for="category in categories" :key="category.idCategory">
+                <div class="flex flex-col items-center justify-center gap-4">
+                    <img :src="category.strCategoryThumb" :alt="category.strCategory"
+                        class="rounded-full h-24 w-24 object-cover shrink-0" />
+                    <h3 class="text-xl font-semibold uppercase">{{ category.strCategory }}</h3>
+                </div>
+            </swiper-slide>
+        </swiper>
     </div>
 </template>
