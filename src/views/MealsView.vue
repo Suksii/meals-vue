32c6740673a7f34f45d0b535e33f31f87ce5338a
unknown <ramovic225@gmail.com>
2025-01-18 15:57:56 +0100

    meal card style modified

--- src/views/MealsView.vue
+++ src/views/MealsView.vue
@@ -31,14 +31,14 @@ watch(letter, fetchMeals)
         </router-link>
     </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <div v-for="meal in mealsByFirstLetter" :key="meal.idMeal" class="w-fit overflow-hidden group rounded-lg">
+        <div v-for="meal in mealsByFirstLetter" :key="meal.idMeal" class="w-fit overflow-hidden group rounded-lg pt-12">
             <router-link :to="`/meal/${meal.idMeal}`" class="relative">
-                <img :src="meal.strMealThumb" :alt="meal.strMeal" class="object-cover group-hover:scale-110 transform duration-300" />
-                <div class="absolute inset-0 bg-black bg-opacity-30"></div>
+                <img :src="meal.strMealThumb" :alt="meal.strMeal"
+                    class="object-cover" />
+                <div class="absolute inset-0 bg-black bg-opacity-30 group-hover:bg-opacity-0 group-hover:scale-50 duration-500"></div>
                 <p
-                    class="absolute top-0 left-1/2 -translate-x-1/2 text-center w-full text-2xl group-hover:text-blue-200 duration-300 text-white font-semibold py-2">
+                    class="absolute top-0 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-gray-50 shadow-lg shadow-gray-700 text-center w-4/5 text-xl group-hover:bg-gray-300 duration-500 font-semibold py-6">
                     {{ meal.strMeal }}</p>
-
             </router-link>
         </div>
     </div>
