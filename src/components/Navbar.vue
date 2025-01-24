d34998423359bd4b95267080830caf6a4a157ebd
unknown <ramovic225@gmail.com>
2025-01-24 16:50:26 +0100

    navbar modified

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,37 +18,40 @@ const showCategories = () => {
 </script>
 
 <template>
-    <div class="flex flex-col md:flex-row items-center justify-evenly gap-6 px-6 py-4 bg-gray-50 shadow-md rounded-lg">
-        <div class="flex items-center gap-8 group">
-            <div class="relative">
-                <p class="text-lg font-semibold px-4 py-2 md:py-6 cursor-pointer hover:text-blue-600 border-b-2 border-transparent hover:border-blue-600 transition-all duration-300 ease-in-out"
-                    @click="showCategories">
-                    Search by category
-                </p>
-            </div>
-            <div class="relative">
-                <p class="text-lg font-semibold px-4 py-2 md:py-6 cursor-pointer hover:text-blue-600 border-b-2 border-transparent hover:border-blue-600 transition-all duration-300 ease-in-out"
-                    @click="showLetters">
-                    Search by first letter
-                </p>
-                <transition name="fade" enter-active-class="transition duration-300 ease-out transform"
-                    enter-from-class="opacity-0 scale-95 -translate-y-12" enter-to-class="opacity-100 scale-100"
-                    leave-active-class="transition duration-300 ease-in transform"
-                    leave-from-class="opacity-100 scale-100 " leave-to-class="opacity-0 scale-95 translate-y-12">
-                    <div v-if="lettersShown"
-                        class="bg-gray-50 shadow-md rounded-md w-96 absolute top-[100%] mt-6 left-1/2 -translate-x-1/2 grid grid-cols-6 gap-2 p-4 z-50">
-                        <router-link :to="`/meals/letter/${letter}`" v-for="letter in letters" :key="letter"
-                            @click="lettersShown = false"
-                            class="text-lg font-medium text-gray-700 cursor-pointer w-12 h-12 flex justify-center items-center bg-blue-100 hover:bg-blue-500 hover:text-white rounded-full transition-colors duration-300 ease-in-out">
-                            {{ letter }}
-                        </router-link>
-                    </div>
-                </transition>
-            </div>
+    <div class=" bg-gray-50 shadow-md">
+        <div class="flex flex-col md:flex-row w-full md:w-[80%] mx-auto items-center justify-between gap-6 px-6 md:px-0 pb-4 md:py-4">
+            <div class="flex items-center gap-8 group">
+                <div class="relative">
+                    <p class="text-lg font-semibold px-4 py-2 md:py-6 cursor-pointer text-gray-700 hover:text-gray-800 border-b-2 border-transparent hover:border-gray-800 transition-all duration-300 ease-in-out"
+                        @click="showCategories">
+                        Search by category
+                    </p>
+                </div>
+                <div class="relative">
+                    <p class="text-lg font-semibold px-4 py-2 md:py-6 cursor-pointer text-gray-700 hover:text-gray-800 border-b-2 border-transparent hover:border-gray-800 transition-all duration-300 ease-in-out"
+                        @click="showLetters">
+                        Search by first letter
+                    </p>
+                    <transition name="fade" enter-active-class="transition duration-300 ease-out transform"
+                        enter-from-class="opacity-0 scale-95 -translate-y-12" enter-to-class="opacity-100 scale-100"
+                        leave-active-class="transition duration-300 ease-in transform"
+                        leave-from-class="opacity-100 scale-100 " leave-to-class="opacity-0 scale-95 translate-y-12">
+                        <div v-if="lettersShown"
+                            class="bg-gray-50 shadow-md rounded-md w-96 absolute top-[100%] mt-6 left-1/2 -translate-x-1/2 grid grid-cols-6 gap-2 p-4 z-50">
+                            <router-link :to="`/meals/letter/${letter}`" v-for="letter in letters" :key="letter"
+                                @click="lettersShown = false"
+                                class="text-lg font-medium text-gray-700 cursor-pointer w-12 h-12 flex justify-center items-center bg-blue-100 hover:bg-blue-500 hover:text-white rounded-full transition-colors duration-300 ease-in-out">
+                                {{ letter }}
+                            </router-link>
+                        </div>
+                    </transition>
+                </div>
 
+            </div>
+            <div class="w-full md:w-auto">
+                <SearchByName />
+            </div>
         </div>
-        <div class="w-full md:w-auto">
-            <SearchByName />
-        </div>
+
     </div>
 </template>
--- src/components/SearchByName.vue
+++ src/components/SearchByName.vue
@@ -14,16 +14,16 @@ const searchByName = () => {
 
 </script>
 <template>
-    <div class="relative w-full flex">
+    <div class="relative w-full flex justify-center items-center min-w-[400px]">
         <input v-model="searchQuery" placeholder="" id="search"
-            class="peer w-full min-w-[350px] py-2 px-3 bg-gray-200 cursor-default focus:outline-none"
+            class="peer w-full py-2 px-3 bg-gray-200 cursor-default focus:outline-none"
             @keyup.enter="searchByName" />
         <label for="search"
             class="absolute -translate-y-1/2 peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-gray-600 peer-focus:text-gray-900 peer-focus:text-[14px] peer-focus:font-semibold peer-focus:-top-3.5 left-2 px-1.5 transition-all duration-300 z-50"
             :class="searchQuery !== '' ? '-top-3.5 text-[14px] font-semibold' : ''">Search
             by name</label>
         <button @click="searchByName"
-            class="py-2 px-4 bg-gray-700 text-white font-semibold outline-none hover:bg-gray-800 duration-300 transition-all ease-in-out">
+            class="py-2 px-4 bg-gray-700 text-white font-semibold outline-none hover:bg-gray-900 duration-300 transition-all ease-in-out">
             Search
         </button>
     </div>
