b2143b22d58a9e7b3e4d9983cf930e37a086e0e3
unknown <ramovic225@gmail.com>
2025-01-25 18:57:50 +0100

    random meal section styled

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,6 +32,33 @@ const onSlideChange = () => {
 </script>
 
 <template>
+
+    <div class="relative flex items-center justify-center bg-black text-white overflow-hidden min-h-screen"
+        v-for="meal in randomMeal">
+        <div class="absolute inset-0 z-0">
+            <img v-if="meal" :src="meal.strMealThumb" :alt="meal.strMeal"
+                class="w-full h-full object-cover filter brightness-75" />
+            <div class="absolute inset-0 bg-gradient-to-b from-black via-black/70 to-transparent"></div>
+        </div>
+        <div class="relative z-10 text-center px-8 max-w-7xl">
+            <h1
+                class="text-5xl md:text-7xl font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-gray-300 via-gray-400 to-gray-300">
+                This can be your next favorite meal</h1>
+            <p
+                class="my-6 text-lg md:text-xl text-gray-300 font-light italic tracking-wide opacity-80 max-w-2xl mx-auto">
+                Explore the world of flavors and elevate your cooking with delicious
+                recipes from around the
+                globe.</p>
+            <h2 v-if="meal" class="text-3xl md:text-4xl font-semibold text-white mt-6 tracking-widest opacity-90">
+                {{ meal.strMeal }}</h2>
+            <div class="py-6">
+                <router-link :to="`/meal/${meal?.idMeal}`"
+                    class="inline-flex items-center px-8 py-4 text-lg font-semibold text-white bg-gradient-to-bl from-gray-400 to-gray-800 rounded-sm shadow-xl hover:shadow-2xl transition-all duration-300 ease-out transform hover:rotate-3 active:scale-95">
+                    Get Recipe
+                </router-link>
+            </div>
+        </div>
+    </div>
     <div class="flex justify-center gap-4 md:w-[80%] px-4 py-8 mx-auto">
         <swiper :modules="modules" :space-between="50" :loop="true"
             :autoplay="{ delay: 1, disableOnInteraction: false }"
@@ -46,26 +73,5 @@ const onSlideChange = () => {
                 </router-link>
             </swiper-slide>
         </swiper>
-    </div>
-
-    <div class="relative flex items-center justify-center bg-black text-white overflow-hidden min-h-screen"
-        v-for="meal in randomMeal">
-        <div class="absolute inset-0 z-0">
-            <img v-if="meal" :src="meal.strMealThumb" :alt="meal.strMeal"
-                class="w-full h-full object-cover filter brightness-75 transition-all duration-700 ease-in-out transform hover:scale-110" />
-            <div class="absolute inset-0 bg-gradient-to-b from-black via-black/70 to-transparent"></div>
-        </div>
-        <div class="relative z-10 text-center px-8 max-w-7xl">
-            <h1 class="">This can be your next favorite meal </h1>
-            <h2 v-if="randomMeal" class="">
-                {{ meal.strMeal }}
-            </h2>
-            <p class="">
-                Explore the world of flavors and elevate your cooking with delicious recipes from around the globe.
-            </p>
-            <div class="">
-                <router-link :to="`/meal/${meal?.idMeal}`" class="">Get Recipe</router-link>
-            </div>
-        </div>
     </div>
 </template>
