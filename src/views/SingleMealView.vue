59ee2b665f74b53c64ab8723b62c38820d4527e4
unknown <ramovic225@gmail.com>
2025-01-28 09:36:22 +0100

    SingleMealView styled

--- src/views/SingleMealView.vue
+++ src/views/SingleMealView.vue
@@ -27,23 +27,35 @@ const getIngredients = (meal) => {
 }
 </script>
 <template>
-    <div class="" v-for="meal in mealById">
-        <div class="relative w-fit mx-auto my-12">
-            <img :src="meal.strMealThumb" />
-            <p
-                class="absolute left-0 top-0 bg-gray-800 text-2xl px-6 py-1.5 -translate-x-1/3 -translate-y-1/3 text-gray-50 italic">
-                {{ meal.strCategory }}</p>
+    <div class="flex flex-col my-12 w-full md:w-[80%] mx-auto" v-for="meal in mealById">
+        <h1 class="text-3xl font-semibold pb-2 text-center border-b-4 mb-8 border-gray-300">{{ meal.strMeal }}</h1>
+        <div class="flex flex-col md:flex-row justify-center items-start">
+            <div class="relative mx-6">
+                <img :src="meal.strMealThumb" class="w-full h-auto object-cover" />
+                <p
+                    class="absolute left-0 top-0 bg-gray-800 text-2xl px-6 py-1.5 -translate-x-1/3 -translate-y-1/3 text-gray-50 italic">
+                    {{ meal.strCategory }}</p>
+            </div>
+            <div class="mx-6 my-4 md:my-0 md:mx-0">
+                <h2 class="text-2xl font-semibold text-gray-800 mb-4 border-b-2 border-gray-300 pb-2 pl-2 w-[400px]">
+                    Ingredients
+                </h2>
+                <ul class="list-disc pl-5 space-y-1">
+                    <li v-for="(ingredient, index) in getIngredients(meal)" :key="index"
+                        class="lg:text-lg text-gray-700">
+                        {{ ingredient }}
+                    </li>
+                </ul>
+                <a :href="meal.strYoutube" target="_blank"
+                    class="inline-flex items-center w-fit px-8 py-4 mt-4 text-lg font-semibold text-white bg-gradient-to-bl from-gray-400 to-gray-800 rounded-sm shadow-xl hover:shadow-2xl transition-all duration-300 ease-out transform hover:rotate-3 active:scale-95">
+                    Watch on YouTube
+                </a>
+            </div>
         </div>
-        <div class="px-12 py-4">
+        <div class="pt-4">
             <h2 class="text-2xl font-semibold text-gray-800 mb-4 border-b-2 border-gray-300 pb-2 pl-2 w-[400px]">
-                Ingredients
-            </h2>
-            <ul class="list-disc pl-5 space-y-1">
-                <li v-for="(ingredient, index) in getIngredients(meal)" :key="index"
-                    class="text-lg text-gray-700">
-                    {{ ingredient }}
-                </li>
-            </ul>
+                Instructions</h2>
+            <p class="pl-5 text-lg tracking-wide">{{ meal.strInstructions }}</p>
         </div>
     </div>
 </template>
